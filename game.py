74c06aa4461b3654b3cea6cfc637123f7dd3e312
1f604 <1f604@users.noreply.github.com>
2017-09-20 19:58:42 +0100

    Create game.py

---
+++ game.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 20 19:40:14 2017
+
+@author: 1f604
+"""
+
+from random import randint
+def f(j):
+    x = 1
+    for i in range(j):
+        x += randint(0,j)
+    print x
+    return x
+chance = 1
+level = 1
+
+while True:
+    raw_input("press enter for your next reward")
+    reward = f(level+3)
+    print "your reward is: ", reward
+    roll = randint(0,10)
+    if chance > roll:
+        level += 1
+        print "level up! Now level ", level
+    
+    
